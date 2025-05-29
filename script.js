8c8c19d4e4fde31cbb0ae5b12526429d3a7eaca0
P34nuts <frank.horn@mail.de>
2025-05-29 09:04:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('root').innerHTML = `
-  <h1 style="text-align:center;margin-top:20%">Resonanzfall – Protokoll I</h1>
-  <p style="text-align:center;color:gray;">GitHub Pages Setup aktiv – Inhalte folgen durch Deployment.</p>
-`;
+console.log('Resonanzfall-Seite aktiv');
